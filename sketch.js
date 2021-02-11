8f504382f5134bb0aa9045f7179f8bcb1602c505
zushenglu <58179949+zushenglu@users.noreply.github.com>
2021-02-11 14:09:58 -0600

    added some idea

--- sketch.js
+++ sketch.js
@@ -1,10 +1,29 @@
 // puzzle maker
+// cool library https://brm.io/matter-js/ 
+// Mappa could be used to produce the maze
+// p5.tiled map could also work
+// 
+// get map factor from open gaming art
+
+
+let mazeWidth,mazeHeight = 9,cellLength = 20;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
+  mazeWidth = width/5;
+  mazeHeight = height/9;
+  cellLength = height/9;
 }
 
 function draw() {
-  background(220);
+  background('violet');
+  drawMaze(mazeWidth,mazeHeight,cellLength);
+}
+
+function drawMaze(width,height,side){
+  for (let y=0;y<height;y++){
+    for (let x=0; x<width;x++){
+      square(x*side,y*side,side);
+    }
+  }
 }
