2bdaf390aa8fc25c4185020ab2b8f60dfb3897df
Zushenglu1809 <Zushenglu1809@gmail.com>
2021-02-19 16:11:24 -0600

    fixed proposal format issue
    also transplanted my python program onto JScode, now it is able to solve the given puzzle.
    
    next step is to build an recursion program that highlights the best route

--- Maze_testing/maze_solver.py
+++ Maze_testing/maze_solver.py
@@ -1,12 +1,12 @@
 # maze
 
-maze = [[0,0,-1,-1,0],
-        [-1,0,-1,0,0],
-        [-1,0,-1,0,-1],
-        [-1,0,0,0,-1],
-        [-1,0,-1,-1,0],
-        [-1,0,0,0,0],
-        [-1,-1,-1,-1,-1]]
+maze = [[0,0,1,1,0],
+        [1,0,1,0,0],
+        [1,0,1,0,1],
+        [1,0,0,0,1],
+        [1,0,1,1,0],
+        [1,0,0,0,0],
+        [1,1,1,1,1]]
 
 # starting at 0,0, end at 4,0
 
@@ -16,17 +16,25 @@ end_point = [4,0]
 def spread(num):
     global maze
     
-    for y in range(len(maze)):
-        for x in range(len(maze[0])):
-            if maze[y][x] == num:
-                if x < len(maze[0]) -1 and maze[y][x+1] == 0:
-                    maze[y][x+1] = num + 1
-                if y < len(maze) - 1 and maze[y+1][x] == 0:
-                    maze[y+1][x] = num + 1
-                if x > 0 and maze[y][x-1] == 0:
-                    maze[y][x-1] = num + 1
-                if y > 0 and maze[y-1][x] == 0:
-                    maze[y-1][x] = num + 1
+    if num == 1:
+        if maze[0][1] == 0:
+            maze[0][1] = num + 1
+        if maze[1][0] == 0:
+            maze[1][0] = num + 1
+
+    
+    else:
+        for y in range(len(maze)):
+            for x in range(len(maze[0])):
+                if maze[y][x] == num:
+                    if x < len(maze[0]) -1 and maze[y][x+1] == 0:
+                        maze[y][x+1] = num + 1
+                    if y < len(maze) - 1 and maze[y+1][x] == 0:
+                        maze[y+1][x] = num + 1
+                    if x > 0 and maze[y][x-1] == 0:
+                        maze[y][x-1] = num + 1
+                    if y > 0 and maze[y-1][x] == 0:
+                        maze[y-1][x] = num + 1
                     
 maze[0][0] = 1 
 
--- sketch.js
+++ sketch.js
@@ -8,22 +8,127 @@
 
 let mazeWidth,mazeHeight = 9,cellLength = 20;
 
+let grid = [
+  [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+  [-1,  1, -1,  0, -1,  0,  0,  0,  0, -1],
+  [-1,  0, -1,  0, -1,  0,  0,  0,  0, -1],
+  [-1,  0, -1,  0, -1, -1, -1, -1,  0, -1],
+  [-1,  0, -1,  0,  0,  0,  0, -1,  0, -1],
+  [-1,  0, -1,  0,  0,  0,  0, -1,  0, -1],
+  [-1,  0,  0,  0,  0,  0,  0, -1,  0, -1],
+  [-1,  0, -1,  0,  0,  0,  0,  0,  0, -1],
+  [-1,  0, -1,  0,  0,  0,  0,  0,  0, -1],
+  [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+]
+
+let bestRouteCoor = []
+
+let start_point = [1,1]
+let end_point = [5,2]
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  mazeWidth = width/5;
-  mazeHeight = height/9;
-  cellLength = height/9;
+  cellLength = 30
 }
 
 function draw() {
   background('violet');
-  drawMaze(mazeWidth,mazeHeight,cellLength);
+  drawMaze(cellLength);
 }
 
-function drawMaze(width,height,side){
-  for (let y=0;y<height;y++){
-    for (let x=0; x<width;x++){
+function drawMaze(side){
+  for (let y=0;y<grid.length;y++){
+    for (let x=0; x<grid[0].length;x++){
+      if (grid[y][x] == -1){
+        fill("brown")
+      }
+      else{
+        fill("white")
+      }
       square(x*side,y*side,side);
+
+      if (grid[y][x] != -1 && grid[y][x] != 0){
+        // here you draw a green circle within the grid, denoting it is calculated
+        fill(0,0,0)
+        text(grid[y][x],x*side + side*2/5, y*side + side*2/3)
+      }
     }
   }
+}
+
+function keyPressed(){
+  if (key == "s"){
+    solveMaze(start_point,end_point,grid)
+    bestRoute(start_point,end_point)
+  }
+}
+
+function solveMaze(start,finish,grid){
+  // yi and xi are initial y and x value, ye and xe are ending x,y value
+  yi = start[1]
+  xi = start[0]
+  ye = finish[1]
+  xe = finish[0] 
+  k = 1
+  while (grid[ye][xe] == 0){
+    spread(k)
+    k += 1
+  }
+}
+
+function spread(num){
+
+  // checking for the number, if white around it, assign it to be next number  
+    for (let y = 0; y < grid.length;y++){
+        for (let x =0; x < grid[0].length;x++){
+            if (grid[y][x] == num){
+                if (x < grid[0].length -1 && grid[y][x+1] == 0){
+                    grid[y][x+1] = num + 1}
+                if (y < grid.length - 1 && grid[y+1][x] == 0){
+                    grid[y+1][x] = num + 1}
+                if (x > 0 && grid[y][x-1] == 0){
+                    grid[y][x-1] = num + 1}
+                if (y > 0 && grid[y-1][x] == 0){
+                    grid[y-1][x] = num + 1}
+                }
+            }
+       }
+    
+}
+
+function bestRoute(start,end){
+  // find the best route: in this case, there are plenty of best
+  // routes, it doesn't matter which one
+  // the idea here is to count backwards: starting at the end
+  // and count the grid that has one less, and eventually get back
+  // to starting point
+  ye = end[1]
+  xe = end[0]
+  yi = start[1]
+  x1 = start[0]
+
+  k = grid[ye][xe];
+
+  countBack(k,xe,ye)
+
+}
+
+function countBack(num,x,y){
+  
+  if (num != 1){
+    if (grid[y][x-1] == num-1){
+      countBack(num-1,y,x-1)
+    }
+    if (grid[y][x+1] == num-1){
+      countBack(num+1,y,x+1)
+    }
+    if (grid[y+1][x] == num-1){
+      countBack(num-1,y+1,x)
+    }
+    if (grid[y-1][x] == num-1){
+      countBack(num-1,y-1,x)
+    }
+  }
+
+ 
 }
